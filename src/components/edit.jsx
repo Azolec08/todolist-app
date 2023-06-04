2ae27146050224ecef3dc1c12441e8f23105e92e
Mark Angelo Celoza <markangeloceloza380@gmail.com>
2023-06-04 14:21:35 +0800

    first-commit

---
+++ src/components/edit.jsx
@@ -0,0 +1,31 @@
+import {useEffect,useRef} from 'react'
+import '../style/edit.scss'
+
+export default function edit({inputEdit,handleCancel,handleUpdate,update,handleOnChangeEdit,setEdit,setInputEdit}) {
+
+  const inputRef = useRef()
+
+  useEffect(() => {
+    inputRef.current.focus()  
+      if(inputEdit.length > 45 ){
+        alert("Input 45 Letters Only")
+        setInputEdit("")
+        setEdit(false)
+        return
+      }
+    
+    
+  })
+  return (
+    <>
+      <div className="border">
+      <h2 style={{margin:"13px 0 0 0 "}}>Edit Wisely!</h2>
+        <div>
+          <input  type="text" name="inputEdit" value={inputEdit} ref={inputRef} onChange={handleOnChangeEdit} placeholder="Input"/>
+          <button onClick={handleCancel} >Cancel</button>
+          <button onClick={() => handleUpdate(update)}>Update</button>
+          </div>
+      </div>
+    </>
+  )
+}
